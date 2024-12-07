7f26d270c670c4610a590ade4a910329324c2210
Ritik Chauhan <critik1704@gmail.com>
2024-12-07 23:09:53 +0530

    removed side bar

--- src/chat/ChatWindow.tsx
+++ src/chat/ChatWindow.tsx
@@ -1,7 +1,6 @@
 import { Message } from './types';
-import {useState, useRef, useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 import MessageBox from './MessageBox';
-import FullAnswer from './FullAnswer';
 import SearchBar from './SearchBar';
 
 export default function ChatWindow({
@@ -11,55 +10,29 @@ export default function ChatWindow({
 }: {
   loading: boolean;
   messages: Message[];
-  sendMessage: (message: string,selectedBot:string) => void;
+  sendMessage: (message: string, selectedBot: string) => void;
 }) {
-  const [showSidebar, setShowSidebar] = useState(false);
-  const [selectedMessage, setSelectedMessage] = useState<Message | null>(null);
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
-  // automatic scroll to bottom of screen when new message is added
+  // Automatic scroll to bottom of screen when a new message is added
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({behavior: 'smooth'});
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages]);
 
-  const handleShowSidebar = (message: Message) => {
-    setSelectedMessage(message);
-    setShowSidebar(true);
-  };
-
-  const handleCloseSidebar = () => {
-    setShowSidebar(false);
-    setSelectedMessage(null);
-  };
-
   return (
-    <div className="w-screen h-screen flex  items-end">
-      <div
-        className={`flex-1 flex flex-col justify-between p-10 pt-0 transition-all duration-300 h-full ${
-          showSidebar ? 'lg:w-2/3' : 'lg:w-full'
-        } items-center`}
-        >
+    <div className="w-screen h-screen flex items-end">
+      <div className="flex-1 flex flex-col justify-between p-10 pt-0 h-full items-center">
         {/* Messages section */}
         <div className="my-10 overflow-y-scroll no-scrollbar w-full lg:w-[60%] h-full">
           {messages?.map((msg, i) => (
-            <MessageBox
-              key={i}
-              message={msg}
-              onShowSidebar={handleShowSidebar}
-            />
+            <MessageBox key={i} message={msg} />
           ))}
           <div ref={messagesEndRef} />
         </div>
-        {/* search bar */}
+
+        {/* Search bar */}
         <SearchBar loading={loading} sendMessage={sendMessage} />
       </div>
-          
-      {/* side bar  */}
-      {showSidebar && selectedMessage && (
-        <div className="w-[40%] h-2/3">
-          <FullAnswer message={selectedMessage} onClose={handleCloseSidebar} />
-        </div>
-      )}
     </div>
   );
 }
--- src/chat/MessageBox.tsx
+++ src/chat/MessageBox.tsx
@@ -1,71 +1,40 @@
-import {Message} from './types';
-import React, {useEffect, useState} from 'react';
+import { Message } from './types';
+import React from 'react';
 import Markdown from 'react-markdown';
-// import {ChatMessageSkeleton} from '../skeletons';
 
 export default function MessageBox({
   message,
-  onShowSidebar,
 }: {
   message: Message;
-  onShowSidebar: (message: Message) => void;
 }) {
-  //trucate message if length > 200 character
-  const truncatedMessage =
-    message.content?.length > 200
-      ? message.content.slice(0, 200) + '...'
-      : message.content;
-
-  const handleShowMoreClick = () => {
-    onShowSidebar(message);
-  };
-
   return (
     <div className="relative w-full">
-      {/* user message */}
+      {/* User message */}
       {message.role === 'user' && (
-        <div className="flex text-end w-full justify-end items-start my-4">
-          <div className="max-w-[50%]  h-max font-outfit  text-sm bg-white px-5 py-3 rounded-2xl mr-2 text-start">
+        <div className="flex justify-end items-start my-4">
+          <div className="max-w-[50%] text-sm bg-white px-5 py-3 rounded-2xl mr-2">
             <p>
               <span className="font-bold">{message.bot} </span>
               {message.content}
             </p>
           </div>
-          <div className="rounded-full flex justify-center items-center bg-zelo-blue-gradient text-xs font-light text-white w-6 h-6 ">
+          <div className="rounded-full flex justify-center items-center bg-blue-500 text-xs text-white w-6 h-6">
             R
           </div>
         </div>
       )}
-      {/* zelo response */}
+
+      {/* Zelo response */}
       {message.role === 'zelo' && (
-        <div className="flex flex-col text-start w-full items-start">
-          <div className="flex w-full">
-            <div className="w-6 h-6 mr-2">
-              
-            </div>
-            <div className="w-full h-max font-outfit text-sm">
-              {/* {message.content === '' ? (
-                <div>Loading...</div>
-              ) : (
-                <> */}
-                  <Markdown>{truncatedMessage}</Markdown>
-                  {/* <Markdown>{message.content}</Markdown> */}
-                {/* </>
-              )} */}
+        <div className="flex flex-col w-full items-start">
+          <div className="flex">
+            <div className="w-full text-sm">
+              {/* <Markdown> */}
+                {message.content}
+                
+              {/* </Markdown> */}
             </div>
           </div>
-          {message.content?.length > 200 && (
-            <div
-              onClick={handleShowMoreClick}
-              className={`${
-                message.content.length < 200 ? 'invisible' : ''
-              } flex gap-2 cursor-pointer border-2 rounded-lg px-2 py-1 ml-8 mt-2`}
-            >
-              <span className="font-normal text-xs text-[#2060AA]">
-                Summary of document
-              </span>
-            </div>
-          )}
         </div>
       )}
     </div>
